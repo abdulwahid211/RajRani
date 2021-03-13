e41747418afdbb2ea6822e824bbf66e61caeae33
Abdul WAhid <you@example.com>
2021-03-13 20:15:34 +0000

    added npm package

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles/styles.scss';
+// import Layout from './components/Layout';
+
+
+
+ReactDOM.render(<h1>Hello RajRani bro</h1>, document.getElementById('app'));
